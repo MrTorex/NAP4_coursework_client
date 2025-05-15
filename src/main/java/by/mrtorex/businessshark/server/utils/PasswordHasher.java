786dbef5cfb0918ede014ab1878fbfcbab3b493d
Loša Ceviel <39222425+MrTorex@users.noreply.github.com>
2025-05-15 17:07:25 +0300

    More securely password hashing system

--- src/main/java/by/mrtorex/businessshark/server/utils/PasswordHasher.java
+++ src/main/java/by/mrtorex/businessshark/server/utils/PasswordHasher.java
@@ -1,22 +1,30 @@
 package by.mrtorex.businessshark.server.utils;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 public class PasswordHasher {
     public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, generateSalt(password));
+    }
+
+    private static String generateSalt(String password) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hashedPassword = digest.digest(password.getBytes());
-            return Base64.getEncoder().encodeToString(hashedPassword);
+
+            StringBuilder salt = new StringBuilder();
+            for (byte b : hashedPassword) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) salt.append('0');
+                salt.append(hex);
+            }
+
+            return "$2a$10$" + salt.substring(0, 22);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static boolean checkPassword(String password, String hashedPassword) {
-        String hashedInputPassword = hashPassword(password);
-        return hashedInputPassword.equals(hashedPassword);
     }
 }
